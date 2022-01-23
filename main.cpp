878158c7758a254ade5e568d7bebe106e3dfefc6
WSH95 <d201780203@hust.edu.cn>
2022-01-23 16:59:58 +0800

    change & complete some functions

--- main.cpp
+++ main.cpp
@@ -7,11 +7,12 @@ int main()
 {
     Timer timer;
     myRoboKine::RobotKinematics kine(0.0838, 0.2, 0.2);
-    Eigen::Vector3d f_pos{0.05, 0.01, -0.03};
+    Eigen::Vector3d f_pos{-0.05, -0.01, 0.2};
     std::vector<Eigen::Vector3d> joint_angle;
 
     timer.start();
-    auto r = kine.calIk_Left(f_pos, joint_angle);
+//    auto r = kine.calIk_Left(f_pos, joint_angle);
+    auto r = kine.calIk_Right(f_pos, joint_angle);
     auto end_t9 = timer.getNs();
     std::cout << "time9: " << end_t9 << " ns." << std::endl;
 
--- robotKinematics.hpp
+++ robotKinematics.hpp
@@ -71,9 +71,13 @@ namespace myRoboKine
 
         Eigen::Vector3d calFk_Left(double theta1_, double theta2_, double theta3_);
 
+        Eigen::Vector3d calFk_Left(Eigen::Vector3d &angles_);
+
         void calFk_Right(Eigen::Vector3d &foot_pos_, double theta1_, double theta2_, double theta3_);
 
         Eigen::Vector3d calFk_Right(double theta1_, double theta2_, double theta3_);
+
+        Eigen::Vector3d calFk_Right(Eigen::Vector3d &angles_);
 
         void calJ_Left(Eigen::Matrix3d &J_, double theta1_, double theta2_, double theta3_);
 
@@ -87,14 +91,20 @@ namespace myRoboKine
          * @brief inverse kinematics of left leg
          * @param foot_pos_ (x, y, z) (unit: m)
          * @param joint_angles_ all solutions. find the nearest solution.
-         * @return negative if no solution
+         * @return bias between new position and given position of foot. positive if no solution
          */
-        double calIk_Left(const Eigen::Vector3d &foot_pos_, std::vector<Eigen::Vector3d> &joint_angles_);
+        double calIk_Left(const Eigen::Vector3d &foot_pos_, std::vector<Eigen::Vector3d> &joint_angles_) const;
 
+        double calIk_Right(const Eigen::Vector3d &foot_pos_, std::vector<Eigen::Vector3d> &joint_angles_) const;
+
+        /// get optimal IK angles concerning current state and joint angle limit.
         double calIkOptimal_Left(const Eigen::Vector3d &foot_pos_, const Eigen::Vector3d &current_angles,
                                  Eigen::Vector3d &joint_angles_);
 
-        double normalizeAngleToPi(double angle_)
+        double calIkOptimal_Right(const Eigen::Vector3d &foot_pos_, const Eigen::Vector3d &current_angles,
+                                  Eigen::Vector3d &joint_angles_);
+
+        static double normalizeAngleToPi(double angle_)
         {
             if ((angle_ >= N_PI) && (angle_ <= PI))
                 return angle_;
@@ -110,11 +120,97 @@ namespace myRoboKine
             }
         }
 
-        void normalizeAngleToPi(Eigen::Vector3d &angle_)
+        static void normalizeAngleToPi(Eigen::Vector3d &angle_)
         {
             angle_[0] = normalizeAngleToPi(angle_[0]);
             angle_[1] = normalizeAngleToPi(angle_[1]);
             angle_[2] = normalizeAngleToPi(angle_[2]);
+        }
+
+        /*!
+         * @brief limit joint angle of left leg in boundary
+         * @param input -pi ~ pi
+         * @param limited -pi ~ pi
+         * @return True if limited == input
+         */
+        bool jointAngleLimit_Left(Eigen::Vector3d &input) const
+        {
+            bool in_boundary = true;
+
+            if (input[0] < low1)
+            {
+                in_boundary = false;
+                input[0] = low1;
+            }
+            else if (input[0] > up1)
+            {
+                in_boundary = false;
+                input[0] = up1;
+            }
+
+            if (input[1] < low2)
+            {
+                in_boundary = false;
+                input[1] = low2;
+            }
+            else if (input[1] > up2)
+            {
+                in_boundary = false;
+                input[1] = up2;
+            }
+
+            if (input[2] < low3)
+            {
+                in_boundary = false;
+                input[2] = low3;
+            }
+            else if (input[2] > up3)
+            {
+                in_boundary = false;
+                input[2] = up3;
+            }
+
+            return in_boundary;
+        }
+
+        bool jointAngleLimit_Right(Eigen::Vector3d &input) const
+        {
+            bool in_boundary = true;
+
+            if (input[0] < -up1)
+            {
+                in_boundary = false;
+                input[0] = -up1;
+            }
+            else if (input[0] > -low1)
+            {
+                in_boundary = false;
+                input[0] = -low1;
+            }
+
+            if (input[1] < low2)
+            {
+                in_boundary = false;
+                input[1] = low2;
+            }
+            else if (input[1] > up2)
+            {
+                in_boundary = false;
+                input[1] = up2;
+            }
+
+            if (input[2] < low3)
+            {
+                in_boundary = false;
+                input[2] = low3;
+            }
+            else if (input[2] > up3)
+            {
+                in_boundary = false;
+                input[2] = up3;
+            }
+
+            return in_boundary;
         }
 
     private:
@@ -125,10 +221,11 @@ namespace myRoboKine
         double x_abs_limit;
     };
 
-    double RobotKinematics::calIk_Left(const Eigen::Vector3d &foot_pos_, std::vector<Eigen::Vector3d> &joint_angles_)
+    double RobotKinematics::calIk_Left(const Eigen::Vector3d &foot_pos_, std::vector<Eigen::Vector3d> &joint_angles_) const
     {
         joint_angles_.clear();
-        double reward = 0;
+//        double reward = 0;
+        double foot_distance_bias;
         Eigen::Vector3d tmp_angle;
         Eigen::Vector3d new_pos;
         new_pos = foot_pos_;
@@ -143,15 +240,15 @@ namespace myRoboKine
         {
             if (new_pos[0] + x_abs_limit < 1.0e-10)
             {
-                reward += pow(100 * (new_pos[0] + x_abs_limit), 2);
+//                reward += pow(100 * (new_pos[0] + x_abs_limit), 2);
                 new_pos[0] = -1.0 * x_abs_limit;
             }
             else if (new_pos[0] - x_abs_limit > -1.0e-10)
             {
-                reward += pow(100 * (new_pos[0] - x_abs_limit), 2);
+//                reward += pow(100 * (new_pos[0] - x_abs_limit), 2);
                 new_pos[0] = x_abs_limit;
             }
-            reward += (pow(100 * new_pos[1], 2) + pow(100 * (new_pos[2] + bound_c_r), 2));
+//            reward += (pow(100 * new_pos[1], 2) + pow(100 * (new_pos[2] + bound_c_r), 2));
             new_pos[1] = 0.0;
             new_pos[2] = -1.0 * bound_c_r;
         }
@@ -160,16 +257,20 @@ namespace myRoboKine
 
         if (distance_origin2foot - bound_s_r_min < 1.0e-10) /// inner small sphere
         {
-            reward = reward + 1.0 * pow(100.0 * (distance_origin2foot - bound_s_r_min), 2);
+//            reward = reward + 1.0 * pow(100.0 * (distance_origin2foot - bound_s_r_min), 2);
             if (bound_s_r_min - l1 < 1.0e-10) /// l2 = l3
             {
-                tmp_angle[0] = atan2(foot_pos_[2], foot_pos_[1]);
+                tmp_angle[0] = atan2(new_pos[2], new_pos[1]);
                 tmp_angle[1] = ANY_ANGLE;
                 tmp_angle[2] = N_PI;
                 normalizeAngleToPi(tmp_angle);
                 joint_angles_.push_back(tmp_angle);
 
-                return -sqrt(reward);
+                foot_distance_bias = (new_pos - foot_pos_).norm();
+
+                return foot_distance_bias;
+
+//                return reward;
             }
             else
             {
@@ -179,7 +280,7 @@ namespace myRoboKine
         }
         else if (distance_origin2foot - bound_s_r_max > -1.0e-10) /// outer max sphere
         {
-            reward = reward + 1.0 * pow(100.0 * (distance_origin2foot - bound_s_r_max), 2);
+//            reward = reward + 1.0 * pow(100.0 * (distance_origin2foot - bound_s_r_max), 2);
             /// projection of the points onto the big sphere.
             new_pos = new_pos * ((bound_s_r_max - 1.0e-7) / distance_origin2foot);
         }
@@ -232,15 +333,159 @@ namespace myRoboKine
         normalizeAngleToPi(tmp_angle);
         joint_angles_.push_back(tmp_angle);
 
-        return -sqrt(reward);
+        foot_distance_bias = (new_pos - foot_pos_).norm();
+
+        return foot_distance_bias;
+
+//        return reward;
+    }
+
+    /// symmetric to the left.
+    double RobotKinematics::calIk_Right(const Eigen::Vector3d &foot_pos_, std::vector<Eigen::Vector3d> &joint_angles_) const
+    {
+        Eigen::Vector3d tmp_pos = {-foot_pos_[0], -foot_pos_[1], foot_pos_[2]};
+
+        double foot_distance_bias = calIk_Left(tmp_pos, joint_angles_);
+
+        for (auto &elem : joint_angles_)
+            elem = -elem;
+
+        return foot_distance_bias;
     }
 
     double RobotKinematics::calIkOptimal_Left(const Eigen::Vector3d &foot_pos_, const Eigen::Vector3d &current_angles,
                                               Eigen::Vector3d &joint_angles_)
     {
+//        double reward = 0;
 
+        std::vector<Eigen::Vector3d> joint_angles_list;
+
+        auto foot_bias = calIk_Left(foot_pos_, joint_angles_list);
+
+        std::vector<Eigen::Vector3d> in_boundary_list, out_boundary_list;
+
+        /// joint angle limit
+        for (auto &elem: joint_angles_list)
+        {
+            bool in_boundary = jointAngleLimit_Left(elem);
+            if (in_boundary)
+                in_boundary_list.push_back(elem);
+            else
+                out_boundary_list.push_back(elem);
+        }
+
+        /// in boundary
+        if (!in_boundary_list.empty())
+        {
+            joint_angles_ = in_boundary_list[0];
+
+            double angle_bias = (joint_angles_ - current_angles).array().abs().sum();
+
+            auto num_in = in_boundary_list.size();
+
+            if (num_in > 1)
+            {
+                for (int i = 1; i < num_in; i++)
+                {
+                    double tmp_bias = (in_boundary_list[i] - current_angles).array().abs().sum();
+                    if (tmp_bias < angle_bias)
+                    {
+                        joint_angles_ = in_boundary_list[i];
+                        angle_bias = tmp_bias;
+                    }
+                }
+            }
+        }
+        else /// out boundary
+        {
+            joint_angles_ = out_boundary_list[0];
+
+            foot_bias = (calFk_Left(joint_angles_) - foot_pos_).norm();
+
+            auto num_out = out_boundary_list.size();
+
+            if (num_out > 1)
+            {
+                for (int i = 1; i < num_out; i++)
+                {
+                    double tmp_bias = (calFk_Left(out_boundary_list[i]) - foot_pos_).norm();
+                    if (tmp_bias < foot_bias)
+                    {
+                        joint_angles_ = out_boundary_list[i];
+                        foot_bias = tmp_bias;
+                    }
+                }
+            }
+        }
+
+        return foot_bias;
+    }
+
+    double RobotKinematics::calIkOptimal_Right(const Eigen::Vector3d &foot_pos_, const Eigen::Vector3d &current_angles,
+                                               Eigen::Vector3d &joint_angles_)
+    {
+        std::vector<Eigen::Vector3d> joint_angles_list;
+
+        auto foot_bias = calIk_Right(foot_pos_, joint_angles_list);
+
+        std::vector<Eigen::Vector3d> in_boundary_list, out_boundary_list;
+
+        /// joint angle limit
+        for (auto &elem: joint_angles_list)
+        {
+            bool in_boundary = jointAngleLimit_Right(elem);
+            if (in_boundary)
+                in_boundary_list.push_back(elem);
+            else
+                out_boundary_list.push_back(elem);
+        }
+
+        /// in boundary
+        if (!in_boundary_list.empty())
+        {
+            joint_angles_ = in_boundary_list[0];
+
+            double angle_bias = (joint_angles_ - current_angles).array().abs().sum();
+
+            auto num_in = in_boundary_list.size();
+
+            if (num_in > 1)
+            {
+                for (int i = 1; i < num_in; i++)
+                {
+                    double tmp_bias = (in_boundary_list[i] - current_angles).array().abs().sum();
+                    if (tmp_bias < angle_bias)
+                    {
+                        joint_angles_ = in_boundary_list[i];
+                        angle_bias = tmp_bias;
+                    }
+                }
+            }
+        }
+        else /// out boundary
+        {
+            joint_angles_ = out_boundary_list[0];
+
+            foot_bias = (calFk_Right(joint_angles_) - foot_pos_).norm();
+
+            auto num_out = out_boundary_list.size();
+
+            if (num_out > 1)
+            {
+                for (int i = 1; i < num_out; i++)
+                {
+                    double tmp_bias = (calFk_Right(out_boundary_list[i]) - foot_pos_).norm();
+                    if (tmp_bias < foot_bias)
+                    {
+                        joint_angles_ = out_boundary_list[i];
+                        foot_bias = tmp_bias;
+                    }
+                }
+            }
+        }
+
+        return foot_bias;
     }
 }
-
 
 #endif //ROBOT_KINEMATICS_ROBOTKINEMATICS_HPP
